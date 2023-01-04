d1c9a069f9e9469a5869872e704682cd6ac47589
Juan Orozco <hi@juanorozco.com>
2023-01-03 22:45:46 -0800

    Improve parsing of photos and title Improved photo parsing, attempting to keep the filetype Added a crappy attempt at parsing the text to detect the title, using the title for the file name and... frontmatter! To that effect, I pulled in the changes from this PR: https://github.com/quantumgardener/dayone-to-obsidian/pull/3 but changed them a bit

--- splitfile.py
+++ splitfile.py
@@ -9,7 +9,7 @@ import shutil
 import time
 
 # Set this as the location where your Journal.json file is located
-root = r"D:\OneDrive\Documents\dayone" 
+root = r"../photo-journal" 
 icons = False   # Set to true if you are using the Icons Plugin in Obsidian
 tagPrefix = "#journal/"  # This will append journal/ as part of the tag name for sub-tags ie. instead of #entry, it is #journal/entry. To exclude set to "". If you change journal to something else, make sure you keep the trailing /
 
@@ -53,59 +53,67 @@ with open(fn, encoding='utf-8') as json_file:
             except KeyError:
                 pass
         location = location[2:]
- 
-        # Add date as page header, removing time if it's 12 midday as time obviously not read
-        if sys.platform == "win32":
-            newEntry.append( '## %s%s\n' % (dateIcon, localDate.strftime( "%A, %#d %B %Y at %#I:%M %p").replace( " at 12:00 PM", "")))
-        else:
-            newEntry.append( '## %s%s\n' % (dateIcon, localDate.strftime( "%A, %-d %B %Y at %-I:%M %p").replace( " at 12:00 PM", "")))  #untested
 
+        dateCreated = str(createDate)
+        coordinates = ''
 
+        frontmatter = ['- created: '+ dateCreated]
+        if 'location' in entry:
+            coordinates = str(entry['location']['latitude']) + ',' + str(entry['location']['longitude'])
+            frontmatter.append('- location: [' + coordinates + ']')
+
+        # newEntry.append(frontmatter)
         # Add body text if it exists (can have the odd blank entry), after some tidying up
         try:
             newText = entry['text'].replace("\\", "")
             newText = newText.replace( "\u2028", "\n")
             newText = newText.replace( "\u1C6A", "\n\n")
+            # Extract title
+            # "^#\s[^#\n]+([\W\w]*?)"gm finds instances of an h1
+            h1s = re.findall(r"^#\s[\w\s]+\n", newText, re.MULTILINE)
+            if h1s.count:
+                title = h1s[0].replace( "\n", "" )
+                title = title.replace( "# ", "" )
+
+                print('Detected title, %s' % title)
+                frontmatter.append('- title: %s' % title)
+            newText = '''---
+''' + '\n'.join(frontmatter) + '''
+---
+
+''' + newText
 
             if 'photos' in entry:
                 # Correct photo links. First we need to rename them. The filename is the md5 code, not the identifier
                 # subsequently used in the text. Then we can amend the text to match. Will only to rename on first run
                 # through as then, they are all renamed.
-                # Assuming all jpeg extensions.
 
                 for p in entry['photos']:
-                    pfn = os.path.join( root, 'photos', '%s.jpeg' % p['md5'] )
+                    pfn = os.path.join( root, 'photos', '%s.%s' % (p['md5'], p['type']) )
+                    print ( 'Checking for %s' % pfn)
                     if os.path.isfile( pfn ):
-                        newfn = os.path.join( root, 'photos', '%s.jpeg' % p['identifier'] )
+                        newfn = os.path.join( root, 'photos', '%s.%s' % (p['identifier'], p['type']) )
                         print ( 'Renaming %s to %s' % (pfn, newfn ))
                         os.rename( pfn, newfn )
 
                 # Now to replace the text to point to the file in obsidian
-                newText = re.sub(r"(\!\[\]\(dayone-moment:\/\/)([A-F0-9]+)(\))", r'![[\2.jpeg]]', newText)
+                newText = re.sub(r"(\!\[\]\(dayone-moment:\/\/)([A-F0-9]+)(\))", r'![[\2.%s]]' % p['type'], newText)
 
             newEntry.append( newText )
         except KeyError:
             pass
 
         ## Start Metadata section
-
-        # newEntry.append( '%%\n' ) #uncomment to hide metadata
-
         newEntry.append( '\n\n---\n' )
-        newEntry.append( '**Metadata**\n')
-        
-        # Add raw create datetime adjusted for timezone and identify timezone
-        newEntry.append( '- Creation Date: %s (%s)\n' % (localDate,entry['timeZone']) )
-        
+
         if not location == '':
             newEntry.append( '- Location: [[%s]]\n' % location) # Remove leading ", "
 
-        # Add GPS, not all entries have this
-        # try:
-        #     newEntry.append( '- GPS: [%s, %s](https://www.google.com/maps/search/?api=1&query=%s,%s)\n' % ( entry['location']['latitude'], entry['location']['longitude'], entry['location']['latitude'], entry['location']['longitude'] ) )
-        # except KeyError:
-        #     pass
-
+            if coordinates == []:
+                locationString =  location 
+            else:
+                locationString = '[' + location + '](geo:' + coordinates + ')'
+            newEntry.append( locationString ) 
 
         tags = []
         if 'tags' in entry:
@@ -130,16 +138,18 @@ with open(fn, encoding='utf-8') as json_file:
             os.mkdir(monthDir)
         
         # Target filename to save to. Will be modified if already exists
-        fnNew = os.path.join( monthDir, "%s.md" % localDate.strftime( '%Y-%m-%d'))
+        if not title:
+            title = localDate.strftime( '%Y-%m-%d')
+        fnNew = os.path.join( monthDir, "%s.md" % title)
         
         # Here is where we handle multiple entries on the same day. Each goes to it's own file
         if os.path.isfile( fnNew):
             # File exists, need to find the next in sequence and append alpha character marker
             index = 97 #ASCII a
-            fnNew = os.path.join( monthDir, "%s%s.md" % (localDate.strftime( '%Y-%m-%d'), chr(index)))
+            fnNew = os.path.join( monthDir, "%s%s.md" % (title, chr(index)))
             while os.path.isfile(fnNew):
                 index += 1
-                fnNew = os.path.join( monthDir, "%s%s.md" % (localDate.strftime( '%Y-%m-%d'), chr(index)))
+                fnNew = os.path.join( monthDir, "%s%s.md" % (title, chr(index)))
 
 
         with open(fnNew, 'w', encoding='utf-8') as f:
